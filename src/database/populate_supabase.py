9063c2b39c09a7a52386c31cb5fe64703ae14299
Filip Topic <filip.topic3@gmail.com>
2025-03-03 23:12:04 +0100

    started work on populate_supabase

---
+++ src/database/populate_supabase.py
@@ -0,0 +1,4 @@
+from config import CONFIG
+
+def process_store_and_populate_supabase(url: str = CONFIG["data_source"]["website"], markdown: str = None):
+    pass
--- src/database/supabase_inserter.py
+++ src/database/supabase_inserter.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from supabase import create_client, Client
+import supabase
 
-"""@dataclass
+@dataclass
 class ProcessedChunk:
     url: str
     chunk_number: int
@@ -23,8 +24,11 @@ def insert_chunk(chunk: ProcessedChunk):
             "embedding": chunk.embedding
         }
 
-        result = supabase.tab
-    except:
-        pass"""
+        result = supabase.table("site_pages").insert(data).execute()
+        print(f"Inserted chunk {chunk.chunk_number} for {chunk.url}")
+        return result
+    except Exception as e:
+        print (f"Error inserting chunk {e}")
+        return None
 
         
